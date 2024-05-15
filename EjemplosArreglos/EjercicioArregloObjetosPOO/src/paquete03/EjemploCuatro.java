b5b3a422602f0ca3adf77a60c721285e23806588
paula226 <palopez25@utpl.edu.ec>
2024-05-15 16:48:32 -0500

    Actualizacion

--- EjemplosArreglos/EjercicioArregloObjetosPOO/src/paquete03/EjemploCuatro.java
+++ EjemplosArreglos/EjercicioArregloObjetosPOO/src/paquete03/EjemploCuatro.java
@@ -8,7 +8,7 @@ package paquete03;
 import paquete01.Calificacion;
 import paquete01.Profesor;
 
-public class Ejemplo {
+public class EjemploCuatro {
     public static void main(String[] args) {
         Profesor profesor1 = new Profesor("Franco", "nombramiento"); 
         Profesor profesor2 = new Profesor("Richard", "contrato"); 
@@ -16,17 +16,20 @@ public class Ejemplo {
         // arreglo estatico
         Calificacion [] calificaciones = new Calificacion[2];
         //
-        Calificacion c = new Calificacion(10, "Computación");
-        Calificacion c2 = new Calificacion(9, "Electrónica");
+        Calificacion c = new Calificacion(10, "Computación",profesor1);
+        Calificacion c2 = new Calificacion(9, "Electrónica", profesor2);
         // calificaciones en un arreglo de tipo de calificacion
         calificaciones[0] = c;
         calificaciones[1] = c2;
         // ciclo repetitivo
         for (int i = 0; i < calificaciones.length; i++) {
             Calificacion objetoCalificacion = calificaciones[i];
-            System.out.printf("%s - %.2f\n", 
+            System.out.printf("%s - %.2f - Profesor(%s)\n", 
                     objetoCalificacion.obtenerNombreMateria(),
-                    objetoCalificacion.obtenerNota());
+                    objetoCalificacion.obtenerNota(),
+                    objetoCalificacion.obtenerProfesor().obtenerNombre());
+                        //al objetoCalificacion llamamos a obtenerProfesor el cula poniendo el punto podemos adquirir otro obtener que nos retorna el nombre de la clase Profesor 
+            
         }
         
     }
